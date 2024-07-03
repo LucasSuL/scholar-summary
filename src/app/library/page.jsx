4b1a9ad95352e3fcb31b9135175e058bbf494b4e
Lucas Su <lijie.su93@gmail.com>
2024-07-03 20:47:29 +0930

    update pages

--- src/app/library/page.jsx
+++ src/app/library/page.jsx
@@ -5,12 +5,11 @@ import Link from "next/link.js";
 
 const Library = () => {
   return (
-    <div>
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 pt-4 my-16">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-5 lg:gap-8">
-          {books.map((book, index) => (
+    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 pt-4 my-16">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-5 lg:gap-8">
+        {books.map((book, index) => (
+          <div key={index}>
             <Link
-              key={index}
               className="group relative block overflow-hidden rounded-lg"
               href={`/library/${index}`}
             >
@@ -22,10 +21,13 @@ const Library = () => {
                 className=" w-full object-cover transition duration-500 group-hover:scale-105  "
               ></Image>
               <div class="absolute inset-0 bg-black bg-opacity-0 transition duration-500 group-hover:bg-opacity-25 "></div>
-
             </Link>
-          ))}
-        </div>
+            <p className="text-sm mt-2 font-bold">{book.title}</p>
+            <p className="text-sm cursor-pointer hover:underline hover:underline-offset-2">
+              {book.author}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
